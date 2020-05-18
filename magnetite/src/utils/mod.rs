89dfb84f3353032961c0a95b7cb94624b82240f2
Stacey Ell <stacey.ell@gmail.com>
2020-05-18 13:20:22 -0600

    add BinStr

--- magnetite/src/utils/mod.rs
+++ magnetite/src/utils/mod.rs
@@ -22,3 +22,34 @@ pub fn hex<'a>(scratch: &'a mut [u8], input: &[u8]) -> Option<&'a str> {
 
     Some(std::str::from_utf8(&scratch[..input.len() * 2]).unwrap())
 }
+
+pub struct BinStr<'a>(pub &'a [u8]);
+
+impl fmt::Debug for BinStr<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "b\"")?;
+        for &b in self.0 {
+            match b {
+                b'\0' => write!(f, "\\0")?,
+                b'\n' => write!(f, "\\n")?,
+                b'\r' => write!(f, "\\r")?,
+                b'\t' => write!(f, "\\t")?,
+                b'\\' => write!(f, "\\\\")?,
+                b'"' => write!(f, "\\\"")?,
+                _ if 0x20 <= b && b < 0x7F => write!(f, "{}", b as char)?,
+                _ => write!(f, "\\x{:02x}", b)?,
+            }
+        }
+        write!(f, "\"")?;
+        Ok(())
+    }
+}
+
+pub struct BinStrBuf(pub Vec<u8>);
+
+impl fmt::Debug for BinStrBuf {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let bin_str = BinStr(&self.0);
+        write!(f, "{:?}.to_vec()", bin_str)
+    }
+}
