8244d672a42f679f02a51ddf6b0bc97fbd4226b8
Stacey Ell <software@e.staceyell.com>
2024-12-15 16:07:05 -0700

    dht: btree-based

--- dht/src/lib.rs
+++ dht/src/lib.rs
@@ -1312,10 +1312,7 @@ impl<'a> fmt::Display for BucketManager2Formatter<'a> {
 
 impl<'a> fmt::Display for BucketInfoFormatter<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut node_count = 0;
-        for _ in self.nodes.clone() {
-            node_count += 1;
-        }
+        let node_count: u32 = self.nodes.clone().map(|_| 1).sum();
         write!(f, "    bucket {}/{} age={:?}    nodes={}\n",
             self.bb.prefix.base.hex(),
             self.bb.prefix.prefix_len,
